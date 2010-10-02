d23d8cb541323eb43f135c345e7442b43320b097
Pierre Habouzit <pierre.habouzit@intersec.com>
2010-10-02 09:47:35 +0200

    Introduce -rewrite-blocks
    Make it use a subset of the ObjC Rewriter.
    
    For now it basically does two things:
    - for one it tries to keep the language the code is written in, whether it
      is C, C++, ObjC or ObjC++, using RAII when the language is C++-based
    - it disables typeof and objc rewriting
    
    Signed-off-by: Pierre Habouzit <pierre.habouzit@intersec.com>

--- include/clang/Frontend/FrontendOptions.h
+++ include/clang/Frontend/FrontendOptions.h
@@ -50,6 +50,7 @@ namespace frontend {
     PrintPreprocessedInput, ///< -E mode.
     RewriteMacros,          ///< Expand macros but not \#includes.
     RewriteObjC,            ///< ObjC->C Rewriter.
+    RewriteBlocks,          ///< C+Blocks -> C Rewriter
     RewriteTest,            ///< Rewriter playground
     RunAnalysis,            ///< Run one or more source code analyses.
     MigrateSource,          ///< Run migrator.
